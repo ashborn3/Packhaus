b93a1833f206fd5add2944fdbdff8a8021bfe2e9
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2025-07-10 06:48:43 +0000

    Refactor main function to initialize configuration, database connection, and set up HTTP router

--- cmd/packhaus/main.go
+++ cmd/packhaus/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"packhaus/internal/api"
+	"packhaus/internal/config"
+	"packhaus/internal/db"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("packhaus!")
+	err := config.LoadConfig()
+	if err != nil {
+		panic("error loading .env: " + err.Error())
+	}
+
+	pool, err := db.Connect()
+	if err != nil {
+		panic("error connecting to db: " + err.Error())
+	}
+	defer pool.Close()
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
+	api.RegisterRoutes(router)
+
+	http.ListenAndServe(":8888", router)
 }
