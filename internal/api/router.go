10f441f5ef4bd717172877018ccc76f3a0de1f18
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2025-07-12 15:59:41 +0000

    Add user authentication route to API

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"packhaus/internal/middleware"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -9,5 +11,22 @@ import (
 func RegisterRoutes(router chi.Router) {
 	router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("{\"ok\": true}"))
+	})
+
+	router.Route("/api", func(r chi.Router) {
+		r.Use(middleware.AuthMiddlware)
+
+		r.Get("/me", func(w http.ResponseWriter, r *http.Request) {
+			val := r.Context().Value(middleware.ContextKeyUserID)
+			userid, ok := val.(string)
+			if !ok || userid == "" {
+				http.Error(w, "unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			fmt.Printf("User ID: %s\n", userid)
+			w.Write([]byte("hello user: " + userid))
+
+		})
 	})
 }
